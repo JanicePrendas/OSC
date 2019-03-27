e4578389be9e163a8e3010ecf46724a284a8393a
Gabriel <g.lizano@hotmail.es>
2019-03-26 23:22:59 -0600

    SignIn y UserRegister

--- app/src/main/java/com/janice/osc/LoginActivity.java
+++ app/src/main/java/com/janice/osc/LoginActivity.java
@@ -1,14 +1,33 @@
 package com.janice.osc;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.janice.osc.Registro.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
-    SignInButton mGoogleSignInButton;
+    private SignInButton mGoogleSignInButton;
+    private TextView mTvRegistrar;
+    private FirebaseAuth mAuth;
+    private EditText mEtCorreo;
+    private EditText mEtContrasena;
+    private Button mBtnIniciarSesion;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +39,34 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         setItems();
         setGoogleButtonText();
+        setViewListeners();
     }
 
     private void setItems(){
         mGoogleSignInButton = findViewById(R.id.sign_in_button);
+        mTvRegistrar = findViewById(R.id.tvRegistrar);
+        mEtContrasena = findViewById(R.id.password_edittext);
+        mEtCorreo = findViewById(R.id.email_edittext);
+        mBtnIniciarSesion = findViewById(R.id.signin_button);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void setViewListeners(){
+        mBtnIniciarSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                autenticar();
+            }
+        });
+
+        mTvRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void setGoogleButtonText(){
@@ -32,5 +75,45 @@ public class LoginActivity extends AppCompatActivity {
         } catch (ClassCastException | NullPointerException e) {
             e.printStackTrace();
         }
+    }
+
+    public void autenticar() {
+        mAuth.signInWithEmailAndPassword(mEtCorreo.getText().toString(), mEtContrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "Authentication good.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void updateUI(FirebaseUser user){
+        if(user!=null){
+            Toast.makeText(this, "Autenticated.",
+                    Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(getApplicationContext(), PanelNavegacion.class);
+//            startActivity(intent);
+        }else{
+            Toast.makeText(this, "Not Autenticated.",
+                    Toast.LENGTH_LONG).show();
+        }
+
     }
 }
--- app/src/main/java/com/janice/osc/Registro/RegisterCustomerFragment.java
+++ app/src/main/java/com/janice/osc/Registro/RegisterCustomerFragment.java
@@ -1,32 +1,121 @@
 package com.janice.osc.Registro;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.janice.osc.R;
 
 public class RegisterCustomerFragment extends Fragment {
+
+    private EditText mNombre_edittext;
+    private EditText mEmail_edittext;
+    private EditText mContrasena_edittext;
+    private EditText mConfirmar_contrasena_edittext;
+    private Button mRegister_button;
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     public RegisterCustomerFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
-    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_customer, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_register_customer, container, false);
+
+        setItems(view);
+        setViewListeners(view);
+
+        return view;
     }
+
+    public void setViewListeners(View view){
+        mRegister_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (validate()) {
+                    registrar();
+                }
+            }
+        });
+    }
+
+    private void setItems(View view){
+        mNombre_edittext = view.findViewById(R.id.nombre_edittext);
+        mEmail_edittext = view.findViewById(R.id.email_edittext);
+        mContrasena_edittext = view.findViewById(R.id.contrasena_edittext);
+        mConfirmar_contrasena_edittext = view.findViewById(R.id.confirmar_contrasena_edittext);
+        mRegister_button = view.findViewById(R.id.register_button);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance("https://osc-app-a1dc6.firebaseio.com/").getReference("usuarios");
+    }
+
+    private boolean validate() {
+        return true;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    public void updateUI(FirebaseUser user){
+        if(user!=null){
+            Toast.makeText(getActivity(), "Autenticated.",
+                    Toast.LENGTH_LONG).show();
+//            Intent intent = new Intent(getApplicationContext(), PanelNavegacion.class);
+//            startActivity(intent);
+        }else{
+            Toast.makeText(getActivity(), "Not Autenticated.",
+                    Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    public void registrar() {
+        String email =mEmail_edittext.getText().toString();
+        String password =mContrasena_edittext.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(getActivity(), new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            DatabaseReference dbusuario = mDatabase.child(user.getUid());
+                            dbusuario.child("nombre").setValue(mNombre_edittext.getText().toString());
+                            updateUI(user);
+                        } else {
+                            Toast.makeText(getActivity(), "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+
+    }
+
+
 
 }
