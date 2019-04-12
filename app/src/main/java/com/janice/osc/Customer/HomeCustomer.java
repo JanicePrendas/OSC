a45205c060e1f3c3f2ae271b90bbd10fb8340605
Janice Prendas <janice.prendas.roldan@gmail.com>
2019-04-12 08:42:07 -0600

    Nombre el Title del Home de Customer

--- app/src/main/java/com/janice/osc/Customer/HomeCustomer.java
+++ app/src/main/java/com/janice/osc/Customer/HomeCustomer.java
@@ -7,6 +7,14 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.janice.osc.Model.Customer;
 import com.janice.osc.R;
 import com.janice.osc.Soda.OrdersFragment;
 import com.janice.osc.Soda.ProductsFragment;
@@ -15,7 +23,10 @@ import com.janice.osc.Util.SectionsPagerAdapter;
 
 public class HomeCustomer extends AppCompatActivity {
 
+    private FirebaseFirestore db;
+    FirebaseUser currentUser;
     ViewPager mViewPager;
+    Customer cliente;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,8 +50,8 @@ public class HomeCustomer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_customer);
 
+        setItems();
         setViewPagerAdapter();
-
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
@@ -53,10 +64,27 @@ public class HomeCustomer extends AppCompatActivity {
 
     private void setupViewPager(ViewPager viewPager) {
         SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
-        adapter.addFragment(new ProductsFragment());
-        adapter.addFragment(new OrdersFragment());
-        adapter.addFragment(new ProfileSodaFragment());
+        adapter.addFragment(new SodasFragment());
+        adapter.addFragment(new CustomerOrdersFragment());
+        adapter.addFragment(new CustomerOrdersFragment());
         viewPager.setAdapter(adapter);
     }
 
+    private void setItems(){
+        db = FirebaseFirestore.getInstance();
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        DocumentReference docRef = db.collection("usuarios").document(currentUser.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        cliente = task.getResult().toObject(Customer.class);
+                        setTitle("Hola, " + cliente.getNombre());
+                    }
+                }
+            }
+        });
+    }
 }
